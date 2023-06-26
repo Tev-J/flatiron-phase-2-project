c90f725bd269426b9a29ec229a782a89d59af11f
Tev-J <tj_delph@yahoo.com>
2023-06-26 03:50:57 -0400

    aligned StyleCards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,20 @@
 import React from "react";
 import StyleCard from "./StyleCard";
+import { Card } from "react-bootstrap";
 
 function Portfolio({ styles }) {
   return (
     <div>
-      <h2>Fresh Cuts Bruh</h2>
-      <>
-        {styles.map((style) => (
-          <StyleCard key={style.id} style={style} />
-        ))}
-      </>
+      <h2 style={{ textAlign: "center" }}>Fresh Cuts Bruh</h2>
+      <div className="container">
+        <div className="row g-3">
+          {styles.map((style) => (
+            <div className="col-12 col-md-6 col-lg-4" key={style.id}>
+              <StyleCard styleDesign={style} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/StyleCard.js
+++ src/components/StyleCard.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-function StyleCard({ style }) {
-  const { id, name, image, likes } = style;
+function StyleCard({ styleDesign }) {
+  const { id, name, image, likes } = styleDesign;
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image} alt={name} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="col-md-4 mb-4">
+      <Card style={{ width: "18rem", height: "100%" }}>
+        <Card.Img
+          variant="top"
+          src={image}
+          alt={name}
+          style={{ objectFit: "cover", height: "70%" }}
+        />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
