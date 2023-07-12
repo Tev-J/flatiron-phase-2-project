45795cd21dc6deda4d9718a64b20e4187aa6edf2
Tev-J <tj_delph@yahoo.com>
2023-07-11 22:58:35 -0400

    added Booking confirmation

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 
 function Bookings() {
@@ -7,18 +8,29 @@ function Bookings() {
   const [timeSlot, setTimeSlot] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [styleChoice, setStyleChoice] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-
     // Reset form fields
+
+    setIsSubmitted(!isSubmitted);
+  }
+
+  function handleBookAnotherAppointment(event) {
+    event.preventDefault();
     setClientName("");
     setDay("");
     setTimeSlot("");
     setPhoneNumber("");
     setStyleChoice("");
-  };
+    setIsSubmitted(!isSubmitted);
+  }
 
+  const goTo = useNavigate();
+  function handleExit() {
+    goTo("/");
+  }
   const daysOfWeek = [
     "Monday",
     "Tuesday",
@@ -48,96 +60,125 @@ function Bookings() {
         pageHeader={"Book Appointment"}
         headerSupport={"We Call. You Confirm."}
       />
-      <div className="container shadow-lg p-4 bg-white rounded">
-        <h3 style={{ textAlign: "center" }}>Enter your preferences.</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-md-6 mb-3">
-              <label htmlFor="day" className="form-label">
-                Day
-              </label>
-              <select
-                className="form-select"
-                id="day"
-                value={day}
-                onChange={(e) => setDay(e.target.value)}
-                required
-              >
-                <option value="">Select a day</option>
-                {daysOfWeek.map((day) => (
-                  <option key={day} value={day}>
-                    {day}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="col-md-6 mb-3">
-              <label htmlFor="timeSlot" className="form-label">
-                Time Slot
-              </label>
-              <select
-                className="form-select"
-                id="timeSlot"
-                value={timeSlot}
-                onChange={(e) => setTimeSlot(e.target.value)}
-                required
-              >
-                <option value="">Select a time slot</option>
-                {timeSlots.map((slot) => (
-                  <option key={slot} value={slot}>
-                    {slot}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="clientName" className="form-label">
-              Client Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="clientName"
-              placeholder="provide your full name"
-              value={clientName}
-              onChange={(e) => setClientName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phoneNumber" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="tel"
-              className="form-control"
-              id="phoneNumber"
-              placeholder="Use dashes. Example: 000-000-0000"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="styleChoice" className="form-label">
-              Style of Choice
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="styleChoice"
-              placeholder=" Unsure? Check out our Inspiration Board!"
-              value={styleChoice}
-              onChange={(e) => setStyleChoice(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-dark col-12">
-            Submit
+      {isSubmitted ? (
+        <div>
+          <h2>Appointment Book</h2>
+          <p>{clientName}'s Appointment Details:</p>
+          <ul>
+            <li>
+              {day} between {timeSlot}
+            </li>
+            <li>Hairstyle: {styleChoice}</li>
+            <br></br>
+            <li>Contact: {phoneNumber}</li>
+          </ul>
+          <button
+            type="submit"
+            className="btn btn-outline-dark"
+            onClick={(e) => handleBookAnotherAppointment(e)}
+          >
+            Book Another Appointment
           </button>
-        </form>
-      </div>
+          <button
+            type="submit"
+            className="btn btn-outline-dark"
+            onClick={(e) => handleExit(e)}
+          >
+            Exit
+          </button>
+        </div>
+      ) : (
+        <div className="container shadow-lg p-4 bg-white rounded">
+          <h3 style={{ textAlign: "center" }}>Enter your preferences.</h3>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="row">
+              <div className="col-md-6 mb-3">
+                <label htmlFor="day" className="form-label">
+                  Day
+                </label>
+                <select
+                  className="form-select"
+                  id="day"
+                  value={day}
+                  onChange={(e) => setDay(e.target.value)}
+                  required
+                >
+                  <option value="">Select a day</option>
+                  {daysOfWeek.map((day) => (
+                    <option key={day} value={day}>
+                      {day}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="col-md-6 mb-3">
+                <label htmlFor="timeSlot" className="form-label">
+                  Time Slot
+                </label>
+                <select
+                  className="form-select"
+                  id="timeSlot"
+                  value={timeSlot}
+                  onChange={(e) => setTimeSlot(e.target.value)}
+                  required
+                >
+                  <option value="">Select a time slot</option>
+                  {timeSlots.map((slot) => (
+                    <option key={slot} value={slot}>
+                      {slot}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="clientName" className="form-label">
+                Client Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="clientName"
+                placeholder="provide your full name"
+                value={clientName}
+                onChange={(e) => setClientName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phoneNumber" className="form-label">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="phoneNumber"
+                placeholder="Use dashes. Example: 000-000-0000"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="styleChoice" className="form-label">
+                Style of Choice
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="styleChoice"
+                placeholder=" Unsure? Check out our Inspiration Board!"
+                value={styleChoice}
+                onChange={(e) => setStyleChoice(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-dark col-12">
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
