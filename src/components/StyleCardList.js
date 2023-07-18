324b2886d7b470f70cadbc0e5d4bd25a11998e60
Tev-J <tj_delph@yahoo.com>
2023-07-18 15:43:37 -0400

    added button to POST

--- src/components/StyleCardList.js
+++ src/components/StyleCardList.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import StyleCard from "./StyleCard";
 import Header from "./Header";
 
 function StyleCardList({ styles }) {
+  const navigate = useNavigate();
+  const handleUploadPhoto = () => {
+    navigate("/community/form");
+  };
+
   return (
     <div>
       <Header
         pageHeader={"Inspire. Be Inspired"}
         headerSupport={"Endure Change, Real Change. Design Yourself."}
       />
+      <div className="d-flex justify-content-center ">
+        <button
+          className="btn btn-primary btn-lg mb-5"
+          type="button"
+          onClick={handleUploadPhoto}
+        >
+          Upload Your Own Style
+        </button>
+      </div>
       <div className="container shadow-lg p-3 mb-5 bg-light rounded">
         <div className="row g-3 mt-4">
           {styles.map((style) => (
